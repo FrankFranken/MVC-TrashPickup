890498ce3ae2b0fe5ed8e23ae104c65a85968235
FrankFranken <Frankfranken1985@gmail.com>
2017-01-12 07:51:18 -0600

    created project

---
+++ Mvc Trash Pickup/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Trash_Pickup
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Mvc Trash Pickup/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc_Trash_Pickup.Startup))]
+namespace Mvc_Trash_Pickup
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
