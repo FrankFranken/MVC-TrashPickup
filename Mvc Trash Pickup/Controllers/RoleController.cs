7edc1cdd6c5b0bf980c9b4620e446b51e2e0b12b
koudk <koudk@LAPTOP-1L7OFQDD>
2017-01-19 09:48:23 -0600

    update

--- Mvc Trash Pickup/Controllers/RoleController.cs
+++ Mvc Trash Pickup/Controllers/RoleController.cs
@@ -1,58 +1,58 @@
-﻿//using Microsoft.AspNet.Identity;
-//using Microsoft.AspNet.Identity.EntityFramework;
-//using Mvc_Trash_Pickup.Models;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
-//using System.Web.Security;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Mvc_Trash_Pickup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
 
-//namespace Mvc_Trash_Pickup.Controllers
-//{
-//    public class RoleController : Controller
-//    {
+namespace Mvc_Trash_Pickup.Controllers
+{
+    public class RoleController : Controller
+    {
 
-//        // GET: Role
-//        public ActionResult Index()
-//        {
-//            ApplicationDbContext context = new ApplicationDbContext();
+        // GET: Role
+        public ActionResult Index()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
 
-//            if (User.Identity.IsAuthenticated)
-//            {
-//                if (!isEmployeeUser())
-//                {
-//                    return RedirectToAction("Index", "Home");
-//                }
-//            }
-//            else
-//            {
-//                return RedirectToAction("Index", "Home");
-//            }
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!isEmployeeUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
 
-//            var roles = context.Roles.ToList();
-//            return View(roles);
-//        }
+            var roles = context.Roles.ToList();
+            return View(roles);
+        }
 
-//        public bool isEmployeeUser()
-//        {
-//            if (User.Identity.IsAuthenticated)
-//            {
-//                var user = User.Identity;
-//                ApplicationDbContext context = new ApplicationDbContext();
-//                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-//                var s = userManager.GetRoles(user.GetUserId());
-//                if (s[0] == "Employee")
-//                {
-//                    return true;
-//                }
-//                else
-//                {
-//                    return false;
-//                }
-//            }
-//            return false;
-//        }
+        public bool isEmployeeUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = userManager.GetRoles(user.GetUserId());
+                if (s[0] == "Employee")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
 
-//    }
-//}
+    }
+}
--- Mvc Trash Pickup/Models/UserType.cs
+++ Mvc Trash Pickup/Models/UserType.cs
@@ -5,12 +5,14 @@ using System.Web;
 
 namespace Mvc_Trash_Pickup.Models
 {
-    public class MemberType
+    public class UserType
     {
         public int Id { get; set; }
 
         public string MemberName { get; set; }
 
         public double PickUpRate { get; set; }
+
+        public double PickUpRoutes { get; set; }
     }
 }
